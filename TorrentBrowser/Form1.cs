361e90c75aa7eff4a1eefab811738e7efe69f3d7
Mike <mikeundead@googlemail.com>
2016-12-19 02:19:47 +0100

    init

---
+++ TorrentBrowser/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+
+namespace TorrentBrowser
+{
+    public partial class Form1 : Form
+    {
+        const string DownloadPage = "https://www.nyaa.se/?page=download&";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void downloadCurrentSearchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string sourcePage = webBrowser1.DocumentText;
+
+            List<string> torrents = GetTorrents(sourcePage);
+            DownloadTorrents(torrents);
+        }
+
+        private static List<string> GetTorrents(string sourcePage)
+        {
+            List<string> torrents = new List<string>();
+            int index = 0;
+            do
+            {
+                index = sourcePage.IndexOf("tid=", index);
+                if (index != -1)
+                {
+                    var endOfTid = sourcePage.IndexOf("\"", index);
+                    var difference = endOfTid - index;
+                    var tid = sourcePage.Substring(index, difference);
+
+                    torrents.Add(DownloadPage + tid);
+                    index++;
+                }
+            } while (index != -1);
+
+            torrents = torrents.Distinct().ToList();
+            return torrents;
+        }
+
+        private static void DownloadTorrents(List<string> torrents)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                int count = 1;
+                foreach (var torrent in torrents)
+                {
+                    string name = $"{count}.torrent";
+                    webClient.DownloadFile(torrent, name);
+                    Process.Start(name);
+                    count++;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.nyaa.se/?page=search&cats=1_37&filter=2");
+        }
+    }
+}
---
+++ TorrentBrowser/Form1.designer.cs
@@ -0,0 +1,90 @@
+﻿namespace TorrentBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.downloadCurrentSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 24);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1384, 736);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.downloadCurrentSearchToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1384, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // downloadCurrentSearchToolStripMenuItem
+            // 
+            this.downloadCurrentSearchToolStripMenuItem.Name = "downloadCurrentSearchToolStripMenuItem";
+            this.downloadCurrentSearchToolStripMenuItem.Size = new System.Drawing.Size(151, 20);
+            this.downloadCurrentSearchToolStripMenuItem.Text = "Download current results";
+            this.downloadCurrentSearchToolStripMenuItem.Click += new System.EventHandler(this.downloadCurrentSearchToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 760);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Torrent Browser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem downloadCurrentSearchToolStripMenuItem;
+    }
+}
+
